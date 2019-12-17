6c56ba353943a7dfec63a37bf0d103060e61f577
Angelos Chalaris <chalarangelo@gmail.com>
2019-12-17 10:17:13 +0200

    Language Fix

---
+++ LanguageFix.cs
@@ -0,0 +1,7 @@
+public class _30s
+{
+    public static void Main(string[] args)
+    {
+       Console.WriteLine("This file is here only to tag the repository language. Do not delete, please!");
+    }
+}
