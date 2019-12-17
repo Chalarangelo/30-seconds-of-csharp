91add92920114ad57b8288045153bfa8f1f50c58
Angelos Chalaris <chalarangelo@gmail.com>
2019-12-17 09:57:00 +0200

    Initialize content repository

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "name": "30-seconds-of-csharp",
+  "description": "Short C# code snippet for all your development needs",
+  "repositoryUrl": "https://github.com/30-seconds/30-seconds-of-csharp",
+  "snippetPath": "snippets",
+  "snippetDataPath": "snippet_data",
+  "language": {
+    "short": "csharp",
+    "long": "C#"
+  }
+};
---
+++ snippet-template.md
@@ -0,0 +1,21 @@
+---
+title: MyFunction
+tags: utility,intermediate
+---
+
+Explain briefly what the snippet does.
+
+Explain briefly how the snippet works.
+
+```csharp
+public static partial class _30s 
+{
+	public static void MyFunction() {
+    // Your code here
+	}
+}
+```
+
+```csharp
+_30s.MyFunction(); // Result
+```
